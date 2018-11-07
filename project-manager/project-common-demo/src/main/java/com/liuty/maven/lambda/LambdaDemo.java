910c3f6ce179a7bd6324d2de238d63adc193a314
ltybjeagle <lty.bjeagle.net@163.com>
2018-11-07 15:51:40 +0800

    Lambda方法引用

--- project-manager/project-common-demo/src/main/java/com/liuty/maven/lambda/LambdaDemo.java
+++ project-manager/project-common-demo/src/main/java/com/liuty/maven/lambda/LambdaDemo.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
@@ -20,6 +23,13 @@ public class LambdaDemo {
         List<String> listOfString = new ArrayList<>();
         Predicate<String> nonEmptyStringPredicate = (String s) -> !s.isEmpty();
         List<String> nonList = filter(listOfString, nonEmptyStringPredicate);
+        forEach(Arrays.asList(1,2,3,4,5), (Integer i) -> System.out.println(i));
+        List<Integer> resultI = map(Arrays.asList("test1", "test2", "test3"), (String s) -> s.length());
+        // 方法引用
+        List<String> testList = Arrays.asList("a", "b", "A", "B");
+        testList.sort((String s1, String s2) -> s1.compareToIgnoreCase(s2));  // 函数式
+        testList.sort((s1, s2) -> s1.compareToIgnoreCase(s2));  // 函数式   类型推断
+        testList.sort(String :: compareToIgnoreCase);  // 方法引用
     }
 
     /**
@@ -47,6 +57,34 @@ public class LambdaDemo {
             if (predicate.test(t)) {
                 result.add(t);
             }
+        }
+        return result;
+    }
+
+    /**
+     * JDK封装函数式接口Consumer   函数签名  void accept(T)
+     * @param list
+     * @param consumer
+     * @param <T>
+     */
+    public static <T> void  forEach(List<T> list, Consumer<T> consumer) {
+        for (T t : list) {
+            consumer.accept(t);
+        }
+    }
+
+    /**
+     * JDK封装函数式接口Function   函数签名  R apply(L)
+     * @param list
+     * @param function
+     * @param <L>
+     * @param <R>
+     * @return
+     */
+    public static <L, R> List<R> map(List<L> list, Function<L, R> function) {
+        List<R> result = new ArrayList<>();
+        for (L l : list) {
+            result.add(function.apply(l));
         }
         return result;
     }
