c117504bee113ba5312bbae3c53276852992f697
ltybjeagle <lty.bjeagle.net@163.com>
2018-11-07 10:30:26 +0800

    Security服务接口增加校验

--- project-manager/project-service-springcloud/src/main/java/com/liuty/maven/rest/DemoUserRestful.java
+++ project-manager/project-service-springcloud/src/main/java/com/liuty/maven/rest/DemoUserRestful.java
@@ -3,9 +3,14 @@ package com.liuty.maven.rest;
 import com.liuty.maven.dao.jpa.DemoUserRepository;
 import com.liuty.maven.dto.Fasp_T_Causer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class DemoUserRestful {
@@ -15,6 +20,16 @@ public class DemoUserRestful {
 
     @GetMapping("{id}")
     public Fasp_T_Causer findById(@PathVariable String id) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            UserDetails user = (UserDetails) principal;
+            Collection<? extends GrantedAuthority> collection = user.getAuthorities();
+            for (GrantedAuthority c : collection) {
+                System.out.println("username:" + user.getUsername() + "、role:" + c.getAuthority());
+            }
+        } else {
+            System.out.println("not login");
+        }
         System.out.println("======================服务端口：8000");
         Fasp_T_Causer user = demoUserRepository.findOne(id);
         return user;
---
+++ project-manager/project-service-springcloud/src/main/java/com/liuty/maven/security/dto/SecurityUser.java
@@ -0,0 +1,92 @@
+package com.liuty.maven.security.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SecurityUser implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+
+    public SecurityUser(String username, String password, String role) {
+        super();
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public SecurityUser() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        GrantedAuthority authority = new SimpleGrantedAuthority(this.role);
+        authorities.add(authority);
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // 用户是否被锁
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // 是否可用
+        return true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
