356dcfffd689d86a2a1cd2bfc255be3c1312e391
ltybjeagle <lty.bjeagle.net@163.com>
2018-11-15 19:03:24 +0800

    用户服务配置调整

--- project-manager/project-service-springcloud/src/main/java/com/liuty/maven/rest/DemoUserRestful.java
+++ project-manager/project-service-springcloud/src/main/java/com/liuty/maven/rest/DemoUserRestful.java
@@ -2,18 +2,22 @@ package com.liuty.maven.rest;
 
 import com.liuty.maven.dao.jpa.DemoUserRepository;
 import com.liuty.maven.dto.Fasp_T_Causer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DemoUserRestful {
 
+    private static final Logger logger = LoggerFactory.getLogger(DemoUserRestful.class);
+
     @Autowired
     private DemoUserRepository demoUserRepository;
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Fasp_T_Causer findById(@PathVariable String id) {
-        System.out.println("======================服务端口：8000");
+        logger.info("======================服务端口：8000");
         Fasp_T_Causer user = demoUserRepository.findOne(id);
         return user;
     }
---
+++ project-manager/project-service-springcloud/src/test/java/com/liuty/maven/test/UserApplicationTest.java
@@ -0,0 +1,43 @@
+package com.liuty.maven.test;
+
+import com.liuty.maven.ProviderUserApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ProviderUserApplication.class)
+public class UserApplicationTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserApplicationTest.class);
+
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void findByIdTest() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.get("/844463533D391D932801CA8806F60516")
+                        .accept(MediaType.APPLICATION_JSON)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        String content = mvcResult.getResponse().getContentAsString();
+        logger.info("请求状态：{}, 请求内容：{}", status, content);
+    }
+}
