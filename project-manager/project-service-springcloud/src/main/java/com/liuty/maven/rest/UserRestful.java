51255c96e02da5478a172168f5c6e1165a4f9fb8
ltybjeagle <lty.bjeagle.net@163.com>
2018-11-20 19:02:04 +0800

    服务端修改

--- project-manager/project-service-springcloud/src/main/java/com/liuty/maven/rest/UserRestful.java
+++ project-manager/project-service-springcloud/src/main/java/com/liuty/maven/rest/UserRestful.java
@@ -2,10 +2,10 @@ package com.liuty.maven.rest;
 
 import com.liuty.maven.dao.jpa.UserRepository;
 import com.liuty.maven.dto.Fasp_T_Causer;
-import com.liuty.maven.log.LoggerLevel;
-import com.liuty.maven.log.LoggerUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 @RestController
 public class UserRestful {
@@ -14,8 +14,11 @@ public class UserRestful {
     private UserRepository demoUserRepository;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Fasp_T_Causer findById(@PathVariable String id) {
-        LoggerUtil.logger(LoggerLevel.INFO, () -> "======================》服务调用");
+    public Fasp_T_Causer findById(@PathVariable String id) throws Exception {
+        //LoggerUtil.logger(LoggerLevel.INFO, () -> "======================》服务调用");
+        System.out.println(Thread.currentThread().getName() + "======================》服务调用");
+        int sleepTime = new Random().nextInt(3000);
+        Thread.sleep(sleepTime);
         Fasp_T_Causer user = demoUserRepository.findOne(id);
         return user;
     }
