ed84e88b313e5202a8cc67ab538ed26e041d9217
ltybjeagle <lty.bjeagle.net@163.com>
2019-02-17 19:19:40 +0800

    springboot

---
+++ project-manager/project-common-demo/src/main/java/com/liuty/maven/springboot/SpBootApplication.java
@@ -0,0 +1,20 @@
+package com.liuty.maven.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpBootApplication {
+
+    public static void main(String ...args) {
+        SpringApplication.run(SpBootApplication.class);
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello spring";
+    }
+}
