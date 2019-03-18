1833e30c04d7812e04a2a3b5a679c8517375c2bb
ltybjeagle <lty.bjeagle.net@163.com>
2019-03-18 18:46:49 +0800

    zookeeper笔记整理

---
+++ project-manager/project-register-zookeeper/src/main/java/com/liuty/maven/ZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.liuty.maven;
+
+/**
+ * Zookeeper：分布式协调服务基础组件。
+ *      1、长连接，客户端心跳检测保持连接，超时断开连接
+ *      2、使用ZAB协议，保存CP原则（一致性、分区容错性）
+ */
+public class ZookeeperApplication {
+
+        public static void main(String ...args) {
+
+    }
+}
