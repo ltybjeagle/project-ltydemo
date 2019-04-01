1bf6cb7241868dede9a235c1f988339b183db533
ltybjeagle <lty.bjeagle.net@163.com>
2019-04-02 00:08:41 +0800

    笔记整理

---
+++ project-manager/project-common-demo/src/main/java/com/liuty/maven/basicfeature/OOPDemo.java
@@ -0,0 +1,19 @@
+package com.liuty.maven.basicfeature;
+
+/**
+ * @描述: 面向对象相关
+ *
+ * 位运算：
+ * 公式：
+ *      1、n & (n - 1)：去掉n值二进制位里最后一个1
+ *      2、n & -n：只保留n值二进制位里最后一个1
+ *
+ * @创建人: Sunny
+ * @创建时间: 2019/4/1
+ */
+public class OOPDemo {
+
+    public static void main(String ...args) {
+
+    }
+}
