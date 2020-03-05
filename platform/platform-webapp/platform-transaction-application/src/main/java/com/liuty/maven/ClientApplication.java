3b309588aa19127278d733dc323f20e3441027df
ltybjeagle <lty.bjeagle.net@163.com>
2020-03-05 19:51:59 +0800

    文档整理

---
+++ platform/platform-webapp/platform-transaction-application/src/main/java/com/liuty/maven/ClientApplication.java
@@ -0,0 +1,26 @@
+package com.liuty.maven;
+
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: Sunny
+ * @date: 2019/11/11
+ */
+@SpringBootApplication
+@RestController
+@EnableDistributedTransaction
+public class ClientApplication {
+    public static void main(String ...args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @RequestMapping()
+    public String insertInfo() {
+        return null;
+    }
+}
