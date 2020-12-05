b335aa744cb627cc2765ab7aef3887e62fe115e9
ltybjeagle <lty.bjeagle.net@163.com>
2020-12-05 10:50:36 +0800

    Spring安全认证

--- platform/platform-webapp/platform-spring-security/src/main/java/com/sunny/maven/formDemo/SpringDemoApplication.java
+++ platform/platform-webapp/platform-spring-security/src/main/java/com/sunny/maven/formDemo/SpringDemoApplication.java
@@ -1,4 +1,4 @@
-package com.sunny.maven;
+package com.sunny.maven.formDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
