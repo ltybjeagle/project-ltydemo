8f08e364f32f274f1ca60f5c0e55a944556f8538
ltybjeagle <lty.bjeagle.net@163.com>
2020-12-05 10:40:05 +0800

    Nacos服务应用

---
+++ platform/platform-alicomb/platform-alicomb-nacos-provider/src/main/java/com/sunny/maven/nacos/NacosProviderApplication.java
@@ -0,0 +1,41 @@
+package com.sunny.maven.nacos;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：SUNNY
+ * @date ：Created in 2020/11/16 21:45
+ * @description：服务提供者 nacos启动命令：startup.cmd -m standalone  （单机模式）
+ * @modified By：
+ * @version: 1.0.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderApplication {
+
+    public static void main(String ...args) throws Exception {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+    @RestController
+    @RefreshScope
+    class EchoController {
+
+        @Value("${useLocalCache:false}")
+        private boolean useLocalCache;
+
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            System.out.println("useLocalCache : " + useLocalCache);
+            return "Hello Nacos Discovery " + string;
+        }
+    }
+}
