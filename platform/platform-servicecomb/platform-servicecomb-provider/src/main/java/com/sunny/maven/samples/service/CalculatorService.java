ed630f398bbefd2413001e5baea382c83f8aad22
ltybjeagle <lty.bjeagle.net@163.com>
2020-12-20 15:08:43 +0800

    servicecomb修改

---
+++ platform/platform-servicecomb/platform-servicecomb-provider/src/main/java/com/sunny/maven/samples/service/CalculatorService.java
@@ -0,0 +1,12 @@
+package com.sunny.maven.samples.service;
+
+/**
+ * @author ：SUNNY
+ * @date ：Created in 2020/12/12 20:53
+ * @description：服务接口
+ * @modified By：
+ * @version: 1.0.0
+ */
+public interface CalculatorService {
+    double calculate(double height, double weight);
+}
