1d034dd224cb630d70c97a6e95ebf725f0d8df52
ltybjeagle <lty.bjeagle.net@163.com>
2022-01-12 16:46:56 +0800

    修改

--- platform-basecommon/platform-basecommon-microservice/platform-basecommon-portal/src/main/java/com/sunny/maven/controller/LoginController.java
+++ platform-basecommon/platform-basecommon-microservice/platform-basecommon-portal/src/main/java/com/sunny/maven/controller/LoginController.java
@@ -1,13 +1,12 @@
 package com.sunny.maven.controller;
 
-import com.sunny.maven.facade.api.UserRemoteClient;
+import com.sunny.maven.usercenter.api.UserInfoApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author SUNNY
@@ -18,17 +17,17 @@ import org.springframework.web.client.RestTemplate;
 public class LoginController {
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    private RestTemplate restTemplate;
-    private static final String USER_CENTER = "usercenter-service";
-    private UserRemoteClient userRemoteClient;
+//    private RestTemplate restTemplate;
+//    private static final String USER_CENTER = "usercenter-service";
+    private UserInfoApi userInfoApi;
 
     @GetMapping("/portal/login")
     public String callHello() {
         StopWatch stopWatch = new StopWatch("/portal/login");
         stopWatch.start();
-        String url = String.format("http://%s/user/hello", USER_CENTER);
-        logger.info("restTemplate：{}", restTemplate.getForObject(url, String.class));
-        String result = userRemoteClient.hello();
+//        String url = String.format("http://%s/user/hello", USER_CENTER);
+//        logger.info("restTemplate：{}", restTemplate.getForObject(url, String.class));
+        String result = userInfoApi.getTestUserName();
         logger.info("Feign client：{}", result);
         stopWatch.stop();
         logger.info("result：{}", stopWatch.getTotalTimeMillis());
@@ -37,11 +36,10 @@ public class LoginController {
 
     /**
      * 构造函数
-     * @param restTemplate REST模板
+     * @param userInfoApi 用户中心用户信息API
      */
     @Autowired
-    public LoginController(RestTemplate restTemplate, UserRemoteClient userRemoteClient) {
-        this.restTemplate = restTemplate;
-        this.userRemoteClient = userRemoteClient;
+    public LoginController(UserInfoApi userInfoApi) {
+        this.userInfoApi = userInfoApi;
     }
 }
