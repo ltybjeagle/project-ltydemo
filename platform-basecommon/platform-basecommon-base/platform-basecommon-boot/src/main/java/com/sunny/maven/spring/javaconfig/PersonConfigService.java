8e7732c23752b59c50b586e01e7c3ee1d8805f41
ltybjeagle <lty.bjeagle.net@163.com>
2022-04-08 23:53:33 +0800

    remove

--- platform-basecommon/platform-basecommon-base/platform-basecommon-boot/src/main/java/com/sunny/maven/spring/javaconfig/PersonConfigService.java
+++
@@ -1,9 +0,0 @@
-package com.sunny.maven.spring.javaconfig;
-
-/**
- * @author SUNNY
- * @description: Java Config定义BEAN
- * @create: 2021-07-28 22:17
- */
-public class PersonConfigService {
-}
--- platform-basecommon/platform-basecommon-base/platform-basecommon-boot/src/main/java/com/sunny/maven/spring/javaconfig/User.java
+++
@@ -1,9 +0,0 @@
-package com.sunny.maven.spring.javaconfig;
-
-/**
- * @author SUNNY
- * @description: POJO测试类
- * @create: 2021-07-29 19:13
- */
-public class User {
-}
