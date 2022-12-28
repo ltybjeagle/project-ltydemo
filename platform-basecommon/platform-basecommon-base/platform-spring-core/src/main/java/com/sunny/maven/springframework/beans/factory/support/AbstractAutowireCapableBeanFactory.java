c942073d98fd4bfb4ad419cd727bba4f5448eb16
ltybjeagle <lty.bjeagle.net@163.com>
2022-12-28 23:45:48 +0800

    add

---
+++ platform-basecommon/platform-basecommon-base/platform-spring-core/src/main/java/com/sunny/maven/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -0,0 +1,96 @@
+package com.sunny.maven.springframework.beans.factory.support;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.sunny.maven.springframework.beans.BeansException;
+import com.sunny.maven.springframework.beans.PropertyValue;
+import com.sunny.maven.springframework.beans.PropertyValues;
+import com.sunny.maven.springframework.beans.factory.config.BeanDefinition;
+import com.sunny.maven.springframework.beans.factory.config.BeanReference;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @author SUNNY
+ * @description: 实现默认bean创建的抽象bean工厂超类
+ * @create: 2022-12-15 09:37
+ */
+public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
+
+    private InstantiationStrategy instantiationStrategy = new CglibSubclassingInstantiationStrategy();
+
+    /**
+     * 生成 Bean 的实例对象
+     * @param beanName
+     * @param beanDefinition
+     * @return
+     * @throws BeansException
+     */
+    @Override
+    protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) throws BeansException {
+        Object bean = null;
+        try {
+            bean = createBeanInstance(beanDefinition, beanName, args);
+            // 给 Bean 对象填充属性
+            applyPropertyValues(beanName, bean, beanDefinition);
+        } catch (Exception e) {
+            throw new BeansException("Instantiation of bean failed", e);
+        }
+
+        registerSingleton(beanName, bean);
+        return bean;
+    }
+
+    /**
+     *
+     * @param beanDefinition
+     * @param beanName
+     * @param args
+     * @return
+     */
+    protected Object createBeanInstance(BeanDefinition beanDefinition, String beanName, Object[] args) {
+        Constructor constructorToUse = null;
+        Class<?> beanClass = beanDefinition.getBeanClass();
+        Constructor<?>[] declaredConstructors = beanClass.getDeclaredConstructors();
+        // 构造函数匹配，可扩展参数类型匹配
+        for (Constructor ctor : declaredConstructors) {
+            if (null != args && ctor.getParameterTypes().length == args.length) {
+                constructorToUse = ctor;
+                break;
+            }
+        }
+        return getInstantiationStrategy().instantiate(beanDefinition, beanName, constructorToUse, args);
+    }
+
+    /**
+     * Bean 对象属性填充
+     * @param beanName
+     * @param bean
+     * @param beanDefinition
+     */
+    protected void applyPropertyValues(String beanName, Object bean, BeanDefinition beanDefinition) {
+        try {
+            PropertyValues propertyValues = beanDefinition.getPropertyValues();
+            for (PropertyValue propertyValue : propertyValues.getPropertyValues()) {
+                String name = propertyValue.getName();
+                Object value = propertyValue.getValue();
+                if (value instanceof BeanReference) {
+                    // A 依赖 B，获取 B 的实例化
+                    BeanReference beanReference = (BeanReference) value;
+                    value = getBean(beanReference.getBeanName());
+                }
+                // 属性填充
+                BeanUtil.setFieldValue(bean, name, value);
+            }
+        } catch (Exception e) {
+            throw new BeansException("Error setting property values: " + beanName);
+        }
+    }
+
+    public InstantiationStrategy getInstantiationStrategy() {
+        return instantiationStrategy;
+    }
+
+    public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
+        this.instantiationStrategy = instantiationStrategy;
+    }
+}
