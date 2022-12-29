51415921addb9388f8f413981407523454f61209
ltybjeagle <lty.bjeagle.net@163.com>
2022-12-29 09:58:18 +0800

    add

---
+++ platform-basecommon/platform-basecommon-microservice/platform-basecommon-eureka/src/main/java/com/sunny/maven/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.sunny.maven.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author SUNNY
+ * @description: Eureka注册中心
+ * @create: 2022-11-01 16:26
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
