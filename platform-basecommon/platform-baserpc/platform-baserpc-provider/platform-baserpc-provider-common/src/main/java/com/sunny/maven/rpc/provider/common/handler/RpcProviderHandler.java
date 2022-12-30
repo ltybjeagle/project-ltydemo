8215b7db5c4694841d9c20ab7b51b4b5fffc4cd1
ltybjeagle <lty.bjeagle.net@163.com>
2022-12-30 09:33:52 +0800

    baserpc

--- platform-basecommon/platform-baserpc/platform-baserpc-provider/platform-baserpc-provider-common/src/main/java/com/sunny/maven/rpc/provider/common/handler/RpcProviderHandler.java
+++ platform-basecommon/platform-baserpc/platform-baserpc-provider/platform-baserpc-provider-common/src/main/java/com/sunny/maven/rpc/provider/common/handler/RpcProviderHandler.java
@@ -1,9 +1,20 @@
 package com.sunny.maven.rpc.provider.common.handler;
 
+import com.sunny.maven.rpc.common.helper.RpcServiceHelper;
+import com.sunny.maven.rpc.common.threadpool.ServerThreadPool;
+import com.sunny.maven.rpc.protocol.RpcProtocol;
+import com.sunny.maven.rpc.protocol.enumeration.RpcStatus;
+import com.sunny.maven.rpc.protocol.enumeration.RpcType;
+import com.sunny.maven.rpc.protocol.header.RpcHeader;
+import com.sunny.maven.rpc.protocol.request.RpcRequest;
+import com.sunny.maven.rpc.protocol.response.RpcResponse;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -12,18 +23,82 @@ import java.util.Map;
  * @create: 2022-12-26 17:29
  */
 @Slf4j
-public class RpcProviderHandler extends SimpleChannelInboundHandler<Object> {
+public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<RpcRequest>> {
     private final Map<String, Object> handlerMap;
     public RpcProviderHandler(Map<String, Object> handlerMap) {
         this.handlerMap = handlerMap;
     }
+
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Object o) throws Exception {
-        log.info("RPC提供者收到的数据为====>>> {}", o.toString());
-        log.info("handlerMap中存放的数据如下所示：");
-        for (Map.Entry<String, Object> entry : handlerMap.entrySet()) {
-            log.info("{} === {}", entry.getKey(), entry.getValue());
+    protected void channelRead0(ChannelHandlerContext ctx, RpcProtocol<RpcRequest> protocol) throws Exception {
+        ServerThreadPool.submit(() -> {
+            RpcHeader header = protocol.getHeader();
+            header.setMsgType((byte) RpcType.RESPONSE.getType());
+            RpcRequest request = protocol.getBody();
+            log.debug("Receive request " + header.getRequestId());
+            RpcProtocol<RpcResponse> responseRpcProtocol = new RpcProtocol<>();
+            RpcResponse response = new RpcResponse();
+            try {
+                Object result = handler(request);
+                response.setResult(result);
+                response.setOneway(request.isOneway());
+                response.setAsync(request.isAsync());
+                header.setStatus((byte) RpcStatus.SUCCESS.getCode());
+            } catch (Throwable t) {
+                response.setError(t.toString());
+                header.setStatus((byte) RpcStatus.FAIL.getCode());
+                log.error("RPC Server handle request error",t);
+            }
+            responseRpcProtocol.setHeader(header);
+            responseRpcProtocol.setBody(response);
+            ctx.writeAndFlush(responseRpcProtocol).addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    log.debug("Send response for request " + header.getRequestId());
+                }
+            });
+        });
+    }
+
+    private Object handler(RpcRequest request) throws Throwable {
+        String serviceKey = RpcServiceHelper.buildServiceKey(request.getClassName(), request.getVersion(),
+                request.getGroup());
+        Object serviceBean = handlerMap.get(serviceKey);
+        if (serviceBean == null) {
+            throw new RuntimeException(String.format("service not exist: %s:%s", request.getClassName(),
+                    request.getMethodName()));
         }
-        ctx.writeAndFlush(o);
+        Class<?> serviceClass = serviceBean.getClass();
+        String methodName = request.getMethodName();
+        Class<?>[] parameterTypes = request.getParameterTypes();
+        Object[] parameters = request.getParameters();
+
+        log.debug(serviceClass.getName());
+        log.debug(methodName);
+        if (parameterTypes != null && parameterTypes.length > 0) {
+            for (Class<?> parameterType : parameterTypes) {
+                log.debug(parameterType.getName());
+            }
+        }
+        if (parameters != null && parameters.length > 0) {
+            for (Object parameter : parameters) {
+                log.debug(parameter.toString());
+            }
+        }
+        return invokeMethod(serviceBean, serviceClass, methodName, parameterTypes, parameters);
+    }
+
+    // TODO 目前使用JDK动态代理方式，此处埋点
+    private Object invokeMethod(Object serviceBean, Class<?> serviceClass, String methodName,
+                                Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
+        Method method = serviceClass.getMethod(methodName, parameterTypes);
+        method.setAccessible(true);
+        return method.invoke(serviceBean, parameters);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error("server caught exception", cause);
+        ctx.close();
     }
 }
