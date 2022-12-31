ccd3fd8ccbea412e6e507c1088f5d8d7180527b9
ltybjeagle <lty.bjeagle.net@163.com>
2022-12-31 10:40:54 +0800

    baserpc

---
+++ platform-basecommon/platform-baserpc/platform-baserpc-consumer/platform-baserpc-consumer-common/src/main/java/com/sunny/maven/rpc/consumer/common/RpcConsumer.java
@@ -0,0 +1,93 @@
+package com.sunny.maven.rpc.consumer.common;
+
+import com.sunny.maven.rpc.consumer.common.handler.RpcConsumerHandler;
+import com.sunny.maven.rpc.consumer.common.initializer.RpcConsumerInitializer;
+import com.sunny.maven.rpc.protocol.RpcProtocol;
+import com.sunny.maven.rpc.protocol.request.RpcRequest;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author SUNNY
+ * @description: 服务消费者
+ * @create: 2022-12-30 17:28
+ */
+@Slf4j
+public class RpcConsumer {
+
+    private final Bootstrap bootstrap;
+    private final EventLoopGroup eventLoopGroup;
+
+    private static volatile RpcConsumer instance;
+
+    private static Map<String, RpcConsumerHandler> handlerMap = new ConcurrentHashMap<>();
+
+    private RpcConsumer() {
+        bootstrap = new Bootstrap();
+        eventLoopGroup = new NioEventLoopGroup(4);
+        bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class).handler(new RpcConsumerInitializer());
+    }
+
+    public static RpcConsumer getInstance() {
+        if (instance == null) {
+            synchronized (RpcConsumer.class) {
+                if (instance == null) {
+                    instance = new RpcConsumer();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void close() {
+        eventLoopGroup.shutdownGracefully();
+    }
+
+    public void sendRequest(RpcProtocol<RpcRequest> protocol) throws Exception {
+        // TODO 暂时写死，后续在引入注册中心时，从注册中心获取
+        String serviceAddress = "127.0.0.1";
+        int port = 27880;
+        String key = serviceAddress.concat("_").concat(String.valueOf(port));
+        RpcConsumerHandler handler = handlerMap.get(key);
+        // 缓存中无RpcClientHandler
+        if (handler == null) {
+            handler = getRpcConsumerHandler(serviceAddress, port);
+            handlerMap.put(key, handler);
+        } else if (!handler.getChannel().isActive()) {
+            // 缓存中存在RpcClientHandler，但不活跃
+            handler.close();
+            handler = getRpcConsumerHandler(serviceAddress, port);
+            handlerMap.put(key, handler);
+        }
+        handler.sendRequest(protocol);
+    }
+
+    /**
+     * 创建连接并返回RpcClientHandler
+     * @param serviceAddress
+     * @param port
+     * @return
+     * @throws InterruptedException
+     */
+    private RpcConsumerHandler getRpcConsumerHandler(String serviceAddress, int port) throws InterruptedException {
+        ChannelFuture channelFuture = bootstrap.connect(serviceAddress, port).sync();
+        channelFuture.addListener((ChannelFutureListener) listener -> {
+            if (channelFuture.isSuccess()) {
+                log.info("connect rpc server {} on port {} success.", serviceAddress, port);
+            } else {
+                log.error("connect rpc server {} on port {} failed.", serviceAddress, port);
+                channelFuture.cause().printStackTrace();
+                eventLoopGroup.shutdownGracefully();
+            }
+        });
+        return channelFuture.channel().pipeline().get(RpcConsumerHandler.class);
+    }
+}
--- platform-basecommon/platform-baserpc/platform-baserpc-provider/platform-baserpc-provider-common/src/main/java/com/sunny/maven/rpc/provider/common/handler/RpcProviderHandler.java
+++ platform-basecommon/platform-baserpc/platform-baserpc-provider/platform-baserpc-provider-common/src/main/java/com/sunny/maven/rpc/provider/common/handler/RpcProviderHandler.java
@@ -2,6 +2,7 @@ package com.sunny.maven.rpc.provider.common.handler;
 
 import com.sunny.maven.rpc.common.helper.RpcServiceHelper;
 import com.sunny.maven.rpc.common.threadpool.ServerThreadPool;
+import com.sunny.maven.rpc.constants.RpcConstants;
 import com.sunny.maven.rpc.protocol.RpcProtocol;
 import com.sunny.maven.rpc.protocol.enumeration.RpcStatus;
 import com.sunny.maven.rpc.protocol.enumeration.RpcType;
@@ -13,6 +14,8 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.cglib.reflect.FastClass;
+import net.sf.cglib.reflect.FastMethod;
 
 import java.lang.reflect.Method;
 import java.util.Map;
@@ -24,8 +27,16 @@ import java.util.Map;
  */
 @Slf4j
 public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<RpcRequest>> {
+    /**
+     * 存储服务名称#版本号#分组与对象实例的映射关系
+     */
     private final Map<String, Object> handlerMap;
-    public RpcProviderHandler(Map<String, Object> handlerMap) {
+    /**
+     * 调用采用哪种类型调用真实方法
+     */
+    private final String reflectType;
+    public RpcProviderHandler(String reflectType, Map<String, Object> handlerMap) {
+        this.reflectType = reflectType;
         this.handlerMap = handlerMap;
     }
 
@@ -88,12 +99,54 @@ public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<
         return invokeMethod(serviceBean, serviceClass, methodName, parameterTypes, parameters);
     }
 
-    // TODO 目前使用JDK动态代理方式，此处埋点
     private Object invokeMethod(Object serviceBean, Class<?> serviceClass, String methodName,
                                 Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
+        switch (this.reflectType) {
+            case RpcConstants.REFLECT_TYPE_JDK:
+                return this.invokeJdkMethod(serviceBean, serviceClass, methodName, parameterTypes, parameters);
+            case RpcConstants.REFLECT_TYPE_CGLIB:
+                return this.invokeCglibMethod(serviceBean, serviceClass, methodName, parameterTypes, parameters);
+            default:
+                throw new IllegalArgumentException("not support reflect type");
+        }
+    }
+
+    /**
+     * JDK代理方式
+     * @param serviceBean Bean
+     * @param serviceClass Bean类型
+     * @param methodName 方法名
+     * @param parameterTypes 参数类型
+     * @param parameters 参数
+     * @return 响应结果
+     * @throws Throwable 异常
+     */
+    private Object invokeJdkMethod(Object serviceBean, Class<?> serviceClass, String methodName,
+                                   Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
+        // JDK reflect
+        log.info("use jdk reflect type invoke method ...");
         Method method = serviceClass.getMethod(methodName, parameterTypes);
         method.setAccessible(true);
         return method.invoke(serviceBean, parameters);
+    }
+
+    /**
+     * CGLib代理方式
+     * @param serviceBean Bean
+     * @param serviceClass Bean类型
+     * @param methodName 方法名
+     * @param parameterTypes 参数类型
+     * @param parameters 参数
+     * @return 响应结果
+     * @throws Throwable 异常
+     */
+    private Object invokeCglibMethod(Object serviceBean, Class<?> serviceClass, String methodName,
+                                   Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
+        // CGLib reflect
+        log.info("use cglib reflect type invoke method ...");
+        FastClass serviceFastClass = FastClass.create(serviceClass);
+        FastMethod serviceFastMethod = serviceFastClass.getMethod(methodName, parameterTypes);
+        return serviceFastMethod.invoke(serviceBean, parameters);
     }
 
     @Override
