e6d431bb3617875060510ce092495f56a1b1cfcd
ltybjeagle <lty.bjeagle.net@163.com>
2023-01-01 23:17:53 +0800

    baserpc

---
+++ platform-basecommon/platform-baserpc/platform-baserpc-proxy/platform-baserpc-proxy-api/src/main/java/com/sunny/maven/rpc/proxy/api/object/ObjectProxy.java
@@ -0,0 +1,120 @@
+package com.sunny.maven.rpc.proxy.api.object;
+
+import com.sunny.maven.rpc.protocol.RpcProtocol;
+import com.sunny.maven.rpc.protocol.header.RpcHeaderFactory;
+import com.sunny.maven.rpc.protocol.request.RpcRequest;
+import com.sunny.maven.rpc.proxy.api.consumer.Consumer;
+import com.sunny.maven.rpc.proxy.api.future.RpcFuture;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author SUNNY
+ * @description: 对象代理类
+ * @create: 2023-01-01 21:11
+ */
+@Slf4j
+public class ObjectProxy<T> implements InvocationHandler {
+
+    /**
+     * 接口的Class对象
+     */
+    private Class<T> clazz;
+    /**
+     * 服务版本号
+     */
+    private String serviceVersion;
+    /**
+     * 服务分组
+     */
+    private String serviceGroup;
+    /**
+     * 超时时间，默认15s
+     */
+    private long timeout = 15000;
+    /**
+     * 服务消费者
+     */
+    private Consumer consumer;
+    /**
+     * 序列化类型
+     */
+    private String serializationType;
+    /**
+     * 是否异步调用
+     */
+    private boolean async;
+    /**
+     * 是否单向调用
+     */
+    private boolean oneWay;
+
+    public ObjectProxy(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    public ObjectProxy(Class<T> clazz, String serviceVersion, String serviceGroup, long timeout, Consumer consumer,
+                       String serializationType, boolean async, boolean oneWay) {
+        this.clazz = clazz;
+        this.serviceVersion = serviceVersion;
+        this.serviceGroup = serviceGroup;
+        this.timeout = timeout;
+        this.consumer = consumer;
+        this.serializationType = serializationType;
+        this.async = async;
+        this.oneWay = oneWay;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (Object.class == method.getDeclaringClass()) {
+            String name = method.getName();
+            if ("equals".equals(name)) {
+                return proxy == args[0];
+            } else if ("hashCode".equals(name)) {
+                return System.identityHashCode(proxy);
+            } else if ("toString".equals(name)) {
+                return proxy.getClass().getName() + "@" + Integer.toHexString(System.identityHashCode(proxy)) +
+                        ", with InvocationHandler " + this;
+            } else {
+                throw new IllegalStateException(String.valueOf(method));
+            }
+        }
+        RpcProtocol<RpcRequest> requestRpcProtocol = new RpcProtocol<>();
+
+        requestRpcProtocol.setHeader(RpcHeaderFactory.getRequestHeader(serializationType));
+
+        RpcRequest request = new RpcRequest();
+        request.setVersion(this.serviceVersion);
+        request.setClassName(method.getDeclaringClass().getName());
+        request.setMethodName(method.getName());
+        request.setParameterTypes(method.getParameterTypes());
+        request.setGroup(this.serviceGroup);
+        request.setParameters(args);
+        request.setOneway(oneWay);
+        request.setAsync(async);
+        requestRpcProtocol.setBody(request);
+
+        // Debug
+        log.debug(method.getDeclaringClass().getName());
+        log.debug(method.getName());
+
+        if (method.getParameterTypes() != null && method.getParameterTypes().length > 0) {
+            for (int i = 0; i < method.getParameterTypes().length; ++i) {
+                log.debug(method.getParameterTypes()[i].getName());
+            }
+        }
+
+        if (args != null && args.length > 0) {
+            for (int i = 0; i < args.length; ++i) {
+                log.debug(args[i].toString());
+            }
+        }
+
+        RpcFuture rpcFuture = this.consumer.sendRequest(requestRpcProtocol);
+        return rpcFuture == null ? null : timeout > 0 ? rpcFuture.get(timeout, TimeUnit.MILLISECONDS) : rpcFuture.get();
+    }
+}
