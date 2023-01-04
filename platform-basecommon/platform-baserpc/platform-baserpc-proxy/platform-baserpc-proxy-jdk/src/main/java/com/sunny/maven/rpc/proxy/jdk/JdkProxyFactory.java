a71a92b40781b1961b777cbf3d7e0bb0ee8aec21
ltybjeagle <lty.bjeagle.net@163.com>
2023-01-04 14:27:16 +0800

    baserpc

--- platform-basecommon/platform-baserpc/platform-baserpc-proxy/platform-baserpc-proxy-jdk/src/main/java/com/sunny/maven/rpc/proxy/jdk/JdkProxyFactory.java
+++ platform-basecommon/platform-baserpc/platform-baserpc-proxy/platform-baserpc-proxy-jdk/src/main/java/com/sunny/maven/rpc/proxy/jdk/JdkProxyFactory.java
@@ -1,7 +1,7 @@
 package com.sunny.maven.rpc.proxy.jdk;
 
-import com.sunny.maven.rpc.proxy.api.consumer.Consumer;
-import com.sunny.maven.rpc.proxy.api.object.ObjectProxy;
+import com.sunny.maven.rpc.proxy.api.BaseProxyFactory;
+import com.sunny.maven.rpc.proxy.api.ProxyFactory;
 
 import java.lang.reflect.Proxy;
 
@@ -10,50 +10,10 @@ import java.lang.reflect.Proxy;
  * @description: JDK动态代理
  * @create: 2023-01-01 21:49
  */
-public class JdkProxyFactory<T> {
-    /**
-     * 服务版本号
-     */
-    private String serviceVersion;
-    /**
-     * 服务分组
-     */
-    private String serviceGroup;
-    /**
-     * 超时时间，默认15s
-     */
-    private long timeout = 15000;
-    /**
-     * 服务消费者
-     */
-    private Consumer consumer;
-    /**
-     * 序列化类型
-     */
-    private String serializationType;
-    /**
-     * 是否异步调用
-     */
-    private boolean async;
-    /**
-     * 是否单向调用
-     */
-    private boolean oneWay;
+public class JdkProxyFactory<T> extends BaseProxyFactory<T> implements ProxyFactory {
 
-    public JdkProxyFactory(String serviceVersion, String serviceGroup, long timeout, Consumer consumer,
-                           String serializationType, boolean async, boolean oneWay) {
-        this.serviceVersion = serviceVersion;
-        this.serviceGroup = serviceGroup;
-        this.timeout = timeout;
-        this.consumer = consumer;
-        this.serializationType = serializationType;
-        this.async = async;
-        this.oneWay = oneWay;
-    }
-
+    @Override
     public <T> T getProxy(Class<T> clazz) {
-        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[] {clazz},
-                new ObjectProxy<T>(clazz, serviceVersion, serviceGroup, timeout, consumer, serializationType,
-                        async, oneWay));
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[] {clazz}, objectProxy);
     }
 }
