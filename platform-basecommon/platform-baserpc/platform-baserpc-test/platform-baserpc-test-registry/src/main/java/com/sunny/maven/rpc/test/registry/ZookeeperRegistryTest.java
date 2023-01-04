edaad40d400388ca2c6d9dd6ea7d9a8b57d8a73f
ltybjeagle <lty.bjeagle.net@163.com>
2023-01-04 15:58:40 +0800

    baserpc

---
+++ platform-basecommon/platform-baserpc/platform-baserpc-test/platform-baserpc-test-registry/src/main/java/com/sunny/maven/rpc/test/registry/ZookeeperRegistryTest.java
@@ -0,0 +1,48 @@
+package com.sunny.maven.rpc.test.registry;
+
+import com.sunny.maven.rpc.protocol.meta.ServiceMeta;
+import com.sunny.maven.rpc.registry.api.RegistryService;
+import com.sunny.maven.rpc.registry.api.config.RegistryConfig;
+import com.sunny.maven.rpc.registry.zookeeper.ZookeeperRegistryService;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author SUNNY
+ * @description: 注册测试
+ * @create: 2023-01-03 22:43
+ */
+public class ZookeeperRegistryTest {
+    private RegistryService registryService;
+    private ServiceMeta serviceMeta;
+
+    @Before
+    public void init() throws Exception {
+        RegistryConfig registryConfig = new RegistryConfig("127.0.0.1:2181", "zookeeper");
+        this.registryService = new ZookeeperRegistryService();
+        this.registryService.init(registryConfig);
+        this.serviceMeta = new ServiceMeta(ZookeeperRegistryTest.class.getName(), "1.0.0",
+                "127.0.0.1", 8080, "SUNNY");
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+        this.registryService.register(serviceMeta);
+    }
+
+    @Test
+    public void testUnRegister() throws Exception {
+        this.registryService.unRegister(serviceMeta);
+    }
+
+    @Test
+    public void testDiscovery() throws Exception {
+        this.registryService.discovery(RegistryService.class.getName(), "SUNNY".hashCode());
+    }
+
+    @Test
+    public void testDestroy() throws Exception {
+        this.registryService.destroy();
+    }
+
+}
