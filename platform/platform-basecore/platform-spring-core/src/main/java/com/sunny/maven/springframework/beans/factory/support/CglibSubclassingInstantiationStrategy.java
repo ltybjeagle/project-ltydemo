b16cb6d3cda16f81acb58943c6a3f8c3985a8601
ltybjeagle <lty.bjeagle.net@163.com>
2023-01-14 00:00:33 +0800

    basecore

---
+++ platform/platform-basecore/platform-spring-core/src/main/java/com/sunny/maven/springframework/beans/factory/support/CglibSubclassingInstantiationStrategy.java
@@ -0,0 +1,32 @@
+package com.sunny.maven.springframework.beans.factory.support;
+
+import com.sunny.maven.springframework.beans.BeansException;
+import com.sunny.maven.springframework.beans.factory.config.BeanDefinition;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.NoOp;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @author SUNNY
+ * @description: Cglib 实例化策略
+ * @create: 2022-12-15 10:58
+ */
+public class CglibSubclassingInstantiationStrategy implements InstantiationStrategy {
+    @Override
+    public Object instantiate(BeanDefinition beanDefinition, String beanName, Constructor ctor, Object[] args)
+            throws BeansException {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(beanDefinition.getBeanClass());
+        enhancer.setCallback(new NoOp() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        });
+        if (null == ctor) {
+            return enhancer.create();
+        }
+        return enhancer.create(ctor.getParameterTypes(), args);
+    }
+}
