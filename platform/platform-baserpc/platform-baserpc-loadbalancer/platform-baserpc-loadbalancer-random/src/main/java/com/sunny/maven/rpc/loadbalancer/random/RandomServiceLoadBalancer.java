41f0add41e6886ee905f2d4881068f3867b1e65e
ltybjeagle <lty.bjeagle.net@163.com>
2023-01-14 20:55:12 +0800

    baserpc

---
+++ platform/platform-baserpc/platform-baserpc-loadbalancer/platform-baserpc-loadbalancer-random/src/main/java/com/sunny/maven/rpc/loadbalancer/random/RandomServiceLoadBalancer.java
@@ -0,0 +1,26 @@
+package com.sunny.maven.rpc.loadbalancer.random;
+
+import com.sunny.maven.rpc.loadbalancer.api.ServiceLoadBalancer;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author SUNNY
+ * @description: 基于随机算法的负载均衡策略
+ * @create: 2023-01-05 16:30
+ */
+@Slf4j
+public class RandomServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    @Override
+    public T select(List<T> servers, int hashCode) {
+        log.info("基于随机算法的负载均衡策略");
+        if (servers == null || servers.isEmpty()) {
+            return null;
+        }
+        Random random = new Random();
+        int index = random.nextInt(servers.size());
+        return servers.get(index);
+    }
+}
