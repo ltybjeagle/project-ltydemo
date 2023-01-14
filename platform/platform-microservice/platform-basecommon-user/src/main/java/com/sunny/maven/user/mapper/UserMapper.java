6e63ae8055d9bd9da9cce291c53c1ab2283b4c4c
ltybjeagle <lty.bjeagle.net@163.com>
2023-01-14 22:02:08 +0800

    microservice

---
+++ platform/platform-microservice/platform-basecommon-user/src/main/java/com/sunny/maven/user/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.sunny.maven.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sunny.maven.user.entity.User;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author SUNNY
+ * @description: 用户Mapper
+ * @create: 2022-06-22 23:46
+ */
+public interface UserMapper extends BaseMapper<User> {
+    /**
+     * 根据编码查询用户
+     * @param code 名称
+     * @return User
+     */
+    User selectByUserCode(@Param("code") String code);
+}
