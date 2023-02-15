327cf3ef080fcc22dc655b32fa50230447dada7a
ltybjeagle <lty.bjeagle.net@163.com>
2023-02-15 09:28:05 +0800

    baserpc

---
+++ platform/platform-baserpc/platform-baserpc-provider/platform-baserpc-provider-spring/src/main/java/com/sunny/maven/rpc/provider/spring/RpcSpringServer.java
@@ -0,0 +1,79 @@
+package com.sunny.maven.rpc.provider.spring;
+
+import com.sunny.maven.rpc.annotation.RpcService;
+import com.sunny.maven.rpc.common.helper.RpcServiceHelper;
+import com.sunny.maven.rpc.constants.RpcConstants;
+import com.sunny.maven.rpc.protocol.meta.ServiceMeta;
+import com.sunny.maven.rpc.provider.common.server.base.BaseServer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.Map;
+
+/**
+ * @author SUNNY
+ * @description: 基于Spring启动RPC服务
+ * @create: 2023-02-14 13:58
+ */
+@Slf4j
+public class RpcSpringServer extends BaseServer implements ApplicationContextAware, InitializingBean {
+
+    public RpcSpringServer(String serverAddress, String reflectType, String registryAddress, String registryType,
+                           String registryLoadBalanceType, int heartbeatInterval, int scanNotActiveChannelInterval) {
+        super(serverAddress, reflectType, registryAddress, registryType, registryLoadBalanceType, heartbeatInterval,
+                scanNotActiveChannelInterval);
+    }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.startNettyServer();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        Map<String, Object> serviceBeanMap = applicationContext.getBeansWithAnnotation(RpcService.class);
+        if (MapUtils.isNotEmpty(serviceBeanMap)) {
+            for (Object serviceBean : serviceBeanMap.values()) {
+                RpcService rpcService = serviceBean.getClass().getAnnotation(RpcService.class);
+                ServiceMeta serviceMeta = new ServiceMeta(this.getServiceName(rpcService), rpcService.version(), host,
+                        port, rpcService.group(), this.getWeight(rpcService.weight()));
+                handlerMap.put(RpcServiceHelper.buildServiceKey(serviceMeta.getServiceName(),
+                        serviceMeta.getServiceVersion(), serviceMeta.getServiceGroup()), serviceBean);
+                try {
+                    registryService.register(serviceMeta);
+                } catch (Exception e) {
+                    log.error("rpc server init spring exception:{}", e.getMessage());
+                }
+            }
+        }
+    }
+
+    private int getWeight(int weight) {
+        if (weight < RpcConstants.SERVICE_WEIGHT_MIN) {
+            weight = RpcConstants.SERVICE_WEIGHT_MIN;
+        }
+        if (weight > RpcConstants.SERVICE_WEIGHT_MAX) {
+            weight = RpcConstants.SERVICE_WEIGHT_MAX;
+        }
+        return weight;
+    }
+
+    /**
+     * 获取serviceName
+     */
+    private String getServiceName(RpcService rpcService) {
+        // 优先使用interfaceClass
+        Class clazz = rpcService.interfaceClass();
+        if (clazz == void.class) {
+            return rpcService.interfaceClassName();
+        }
+        String serviceName = clazz.getName();
+        if (serviceName == null || serviceName.trim().isEmpty()) {
+            serviceName = rpcService.interfaceClassName();
+        }
+        return serviceName;
+    }
+}
