d3bd0975775f9eb3d381e2b8b14efde4030a4a5d
ltybjeagle <lty.bjeagle.net@163.com>
2023-06-16 09:27:20 +0800

    代码迁移

---
+++ platform/platform-middle/platform-middle-servlet/src/main/java/com/sunny/maven/middle/MiddleStarter.java
@@ -0,0 +1,16 @@
+package com.sunny.maven.middle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SUNNY
+ * @description: 中台服务
+ * @create: 2023/6/6 10:56
+ */
+@SpringBootApplication
+public class MiddleStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(MiddleStarter.class, args);
+    }
+}
